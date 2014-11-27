c8b237c9657e0e3892e8bb6445471f075db9b27b
Boudewijn <goldenkooy@gmail.com>
2014-11-27 19:52:55 +0100

    site setting aangemaakt....

---
+++ includes/include.Settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * 
+ * todo: define de DOCUMENTROOT en BASEFOLDER
+ * todo: define de DB_HOST,DB_USER,DB_PASS en DB_NAME
+ * 
+ */
+
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
             //AUTOLOADER//
 //alle classes includen uit de classes map
-//todo: define de DOCUMENTROOT en BASEFOLDER
-//todo: classes map maken en classes er in zetten
 
+require('includes/include.Settings.php');
 function __autoload($class_name)
 {
     if (file_exists(DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php"))
