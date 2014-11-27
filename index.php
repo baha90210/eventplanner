28ed345c0c8fd6614876e54e8034ab081232bbbb
Boudewijn <goldenkooy@gmail.com>
2014-11-27 16:53:08 +0100

    Update index.php
    commentaar op de autoloader toegevoegd

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-//alle classes includen
+            //AUTOLOADER//
+//alle classes includen uit de classes map
+//todo: define de DOCUMENTROOT en BASEFOLDER
+//todo: classes map maken en classes er in zetten
+
 function __autoload($class_name)
 {
     if (file_exists(DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php"))
