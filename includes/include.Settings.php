e5196c683b04d440c850e36a5df8a225f8416ecd
Boudewijn <goldenkooy@gmail.com>
2014-11-27 20:19:57 +0100

    settings bestand gemaakt

--- includes/include.Settings.php
+++ includes/include.Settings.php
@@ -2,8 +2,25 @@
 
 /* 
  * 
- * todo: define de DOCUMENTROOT en BASEFOLDER
- * todo: define de DB_HOST,DB_USER,DB_PASS en DB_NAME
+ * define de DOCUMENTROOT
+ * define de DB_HOST,DB_USER,DB_PASS en DB_NAME
  * 
  */
+//Defines hier
+define('DOCUMENTROOT',$_SERVER['DOCUMENT_ROOT']);
+define('BASEFOLDER','/netprojects/eduvisionmasterclass/');
+define('DB_HOST','');
+define('DB_USER','');
+define('DB_PASS','');
+define('DB_NAME','');
+
+            //AUTOLOADER//
+//classes automatisch ophalen uit de classes map
+function __autoload($class_name)
+{
+    if (file_exists(DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php"))
+    {
+        require_once DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php";
+    }
+}
 
--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-            //AUTOLOADER//
-//alle classes includen uit de classes map
-
 require('includes/include.Settings.php');
-function __autoload($class_name)
-{
-    if (file_exists(DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php"))
-    {
-        require_once DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php";
-    }
-}
-echo "Hello classmates";
 ?>
-
+<a href="<?php DOCUMENTROOT; ?>">HOME</a>
