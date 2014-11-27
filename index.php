84da667044b50c1a9bdd7648aa6f6ecc0cf5bbbb
Boudewijn <goldenkooy@gmail.com>
2014-11-27 16:50:09 +0100

    Update index.php
    autoloader voor classes gemaakt in de index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+//alle classes includen
+function __autoload($class_name)
+{
+    if (file_exists(DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php"))
+    {
+        require_once DOCUMENTROOT.BASEFOLDER."classes/class.$class_name.php";
+    }
+}
 echo "Hello classmates";
 ?>
 
