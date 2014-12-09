1e448154541ba120414bbce935cf69dd6e72ecc4
Boudewijn <goldenkooy@gmail.com>
2014-12-09 19:10:21 +0100

    nutteloos object .....foutje

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 // basefolder instelling wordt gedaan in de autoloader
 require('includes/include.Autoloader.php');
 // server settings worden gedaan in de construct van LoadSettings
-
-$page = new LoadSettings;
-
-
+new LoadSettings;
 ?>
 <a href="<?php DOCUMENTROOT; ?>">Eventplanner Home</a><br />
 <?=SERVEROWNER.'<br />'?>
