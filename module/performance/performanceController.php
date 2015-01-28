b8fea3582cb38a922edcdf75a5d5690335b9b189
sander <sander@sander-HP-G72-Notebook-PC>
2015-01-28 11:21:38 +0100

    performance module initial

---
+++ module/performance/performanceController.php
@@ -0,0 +1,103 @@
+<?php
+class performanceController extends Controller{
+	public function __construct(){
+		//parent::__construct();
+		
+		$this->authorize();
+                $this->addScript('./themes/default/javascript/jquery/jquery-1.7.1.min.js');
+	}
+
+	public function overview(){		
+		$this->setTitle('Overzicht performance');
+		
+		$this->loadModel('performance');
+		$this->performance = $this->model->getPerformances();
+                
+                $this->loadModel('event');
+                $this->events = $this->model->getEvents();
+		
+		$this->render('performance_overview.tpl');
+	}
+
+	public function add(){
+		$this->setTitle('Performance toevoegen');
+
+		if($_POST)
+                {
+			$this->loadModel('performance');
+			$this->model->addEvent($_POST);
+			$this->msg = 'Performance '.$_POST['name'].' werd toegevoegd.';			
+			$this->redirect('performance/overview', $_GET['token']);
+		}
+                else
+                {		
+			$this->addScript('./themes/default/javascript/jquery/jquery-1.7.1.min.js');
+
+			$this->event = array(
+				'name'          => '',
+				'start_date'    => '',
+				'end_date'      => '',
+				'price'		=> ''
+			);
+                        $this->event_locations = array();
+                        $this->loadModel('location');
+			$this->locations = $this->model->getLocations();
+			$this->render('performance_detail.tpl');		
+		}
+	}
+
+	public function edit(){
+		$this->setTitle('performance aanpassen');
+
+		$this->loadModel('performance');
+
+		if($_POST)
+                {
+                    if($this->validate($_POST))
+                    {
+			$this->model->editEvent($_POST);
+			$this->msg = 'Performance '.$_POST['req_name'].' werd aangepast.';
+                    }						
+                    $this->overview();
+		}
+		
+		if(isset($_GET['id']))
+                {
+			$id = $_GET['id'];
+			$this->performance = $this->model->getPerformance($id);
+			$this->render('performance_detail.tpl');		
+		}
+	}
+
+	public function delete(){
+		if(isset($_GET['id'])){
+                    
+			$id = $_GET['id'];
+			$this->loadModel('performance');
+			$performance = $this->model->getEvent($id);
+			$this->model->deleteEvent($id);
+			$this->msg = 'U heeft performance '.$event['name'].' verwijderd.';
+		}
+		
+		$this->overview();
+		//$this->redirect('index.php?route=event/overview');
+	}
+	
+	private function validate($data){
+		foreach($data as $k => $v){
+			if(substr($k, 0, 3) == 'req'){
+				if($v == ''){
+					return false;
+				}
+			}
+			
+			if(substr($k, 0, 6) == 'reqnum'){
+				if(!is_numeric($v)){
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+}
---
+++ module/performance/performanceModel.php
@@ -0,0 +1,105 @@
+<?php
+class performanceModel extends Model{
+	
+    public function getPerformances(){
+		$sql = "SELECT performance_id, performance_title, t1.event_id, event.name AS eventname, artist.name AS artistname, t1.date_from, t1.date_until, confirmed ";
+                $sql .= "FROM performance AS t1 ";
+                $sql .= "INNER JOIN artist ON artist.artist_id = t1.artist_id ";
+                $sql .= "INNER JOIN event ON event.event_id = t1.event_id ";
+                if (isset($_GET['event']) && $_GET['event']!=='') $sql .= "WHERE t1.event_id='".$_GET['event']."' ";
+                if (isset($_GET['artist']) && $_GET['artist']!=='') $sql .= "WHERE t1.artist_id='".$_GET['artist']."' ";
+                $sql .= "ORDER BY date_from ASC";
+		
+		$result = $this->db->query($sql);
+		//var_dump($result->rows);
+		return $result->rows;
+	}
+	
+	public function deletePerformance($id){
+		$sql = "DELETE FROM event WHERE event_id = '".$this->db->escape($id)."'";
+		$this->db->query($sql);
+                
+		$sql = "DELETE FROM event_locations WHERE event_id = '".$this->db->escape($id)."'";
+		$this->db->query($sql);
+		return;
+	}
+	
+	public function addEvent($data){
+            if($this->validatePerformance($data))
+            {
+		$sql  = "INSERT INTO event SET ";
+		$sql .= "name = '".$this->db->escape($data['req_name'])."', ";
+		$sql .= "start_date = '".$data['req_start_date']."', ";
+		$sql .= "end_date = '".$data['req_end_date']."', ";
+		$sql .= "price = '".$data['reqnum_price']."'";
+		
+		$this->db->query($sql);
+
+                //handle event locations
+		$sql = "DELETE FROM event_location WHERE event_id = '".$data['id']."'";
+		$this->db->query($sql);
+			
+		foreach($data['location'] as $k=>$v){
+                    $sql = "INSERT IGNORE INTO event_location SET ";
+                    $sql .= "event_id = '".$data['id']."', ";
+                    $sql .= "location_id = '".$v."'";
+		
+                    $this->db->query($sql);
+		}   
+            }
+            return;
+	}
+	
+	public function editPerformance($data){
+		if($this->validatePerformance($data)){
+			//handle core event data
+			$sql  = "UPDATE event SET ";
+			$sql .= "name = '".$this->db->escape($data['req_name'])."', ";
+			$sql .= "start_date = '".$data['req_start_date']."', ";
+			$sql .= "end_date = '".$data['req_end_date']."', ";
+			$sql .= "price = '".$data['reqnum_price']."' ";
+			$sql .= "WHERE event_id = '".$data['id']."'";
+			
+			$this->db->query($sql);
+			
+			//handle event locations
+			$sql = "DELETE FROM event_location WHERE event_id = '".$data['id']."'";
+			$this->db->query($sql);
+			
+			foreach($data['location'] as $k=>$v){
+				$sql = "INSERT IGNORE INTO event_location SET ";
+				$sql .= "event_id = '".$data['id']."', ";
+				$sql .= "location_id = '".$v."'";
+				
+				$this->db->query($sql);
+			}
+		}		
+		return;
+	}
+	
+	public function getPerformance($id)
+        {
+            $sql = "SELECT * FROM performance WHERE performance_id = '".$id."'";
+            $result = $this->db->query($sql);
+		
+            return $result->row;
+	}
+
+	private function validatePerformance($data){
+		foreach($data as $k => $v){
+			if(substr($k, 0, 3) == 'req'){
+				if($v == ''){
+					return false;
+				}
+			}
+			
+			if(substr($k, 0, 6) == 'reqnum'){
+				if(!is_numeric($v)){
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+}
